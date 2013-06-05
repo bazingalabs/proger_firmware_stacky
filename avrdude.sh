712dc79c58d782ed086dd3195fbf6212f24e421b
Erik Kallen <info@erikkallen.nl>
2013-06-05 21:57:58 +0200

    Added hex to gitignore

--- avrdude.sh
+++ avrdude.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-avrdude -cavr109 -patmega328p -P/dev/tty.usbmodem1a1231 -Uflash:w:ReadAnalogVoltage.cpp.hex:i -vvvvvv
+avrdude -cavr109 -patmega328p -P/dev/tty.usbmodem1a1231 -Uflash:w:Blink.cpp.hex:i -vvvvvv
